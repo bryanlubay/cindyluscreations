77f274564c0d205fb554026da1a7818ddb58aec4
lubayb1 <bryanlubay1@gmail.com>
2020-12-25 22:41:58 -0800

    25 Dec House

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
         <div className="background">
         <h1>Welcome to Cindylus Creations!</h1>
-        <p className="menu"><b>Custom</b> &nbsp; &nbsp; &nbsp;<b>Disney</b> &nbsp; &nbsp; &nbsp;<b>Las Vegas Golden Knights</b> &nbsp; &nbsp; &nbsp;<b>Las Vegas Raiders</b> &nbsp; &nbsp; &nbsp;<b>Los Angeles Dodgers</b> &nbsp; &nbsp; &nbsp;<b>Los Angeles Lakers</b> &nbsp; &nbsp; &nbsp;<b>New Years 2021</b></p>
-
+        <p className="menu"><b className="menu-item">Custom</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Disney</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Las Vegas Golden Knights</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Las Vegas Raiders</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Los Angeles Dodgers</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Los Angeles Lakers</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">New Years 2021</b></p>
 
      </div>
   );
