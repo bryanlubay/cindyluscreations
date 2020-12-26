ea25d114249be45ce630f78321a734c7822107c1
lubayb1 <bryanlubay1@gmail.com>
2020-12-25 22:17:44 -0800

    25 Dec House

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
         <div className="background">
         <h1>Welcome to Cindylu's Creations!</h1>
-        <a> <h2>About me</h2 ></a>
-        <a> <h2>Gallery</h2> </a>
-        <a> <h2>Contact</h2> </a>
+
+
+
         </div>
   );
 }
