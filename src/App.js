8f6a3f4c909d88e536a87e07105a21b0210ce495
lubayb1 <bryanlubay1@gmail.com>
2020-12-25 23:20:11 -0800

    25 Dec House

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
         <div className="background">
         <h1>Welcome to Cindylus Creations!</h1>
         <p className="menu"><b className="menu-item">Custom</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Disney</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Movies</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Music</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">New Years 2021</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Sports</b></p>
-        <p>Hi, my name is Cindy and welcome to my website! I am a growing small bussiness looking to build new relationships. </p>
      </div>
   );
 }
