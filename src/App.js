8f0fce639ed55ab96c939b7ffed39b36ea6d2b6b
lubayb1 <bryanlubay1@gmail.com>
2020-12-26 10:17:33 -0800

    26 Dec House

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import InstagramEmbed from 'react-instagram-embed';
 import React from 'react';
 import './App.css';
 
@@ -6,7 +7,23 @@ function App() {
         <div className="background">
         <h1>Welcome to Cindylus Creations!</h1>
         <p className="menu"><b className="menu-item">Custom</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Disney</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Movies</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Music</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">New Years 2021</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Sports</b></p>
+
+        <InstagramEmbed
+  url='https://www.instagram.com/p/CA3aDJ7HlTE/'
+  clientAccessToken='123|456'
+  maxWidth={320}
+  hideCaption={false}
+  containerTagName='div'
+  protocol=''
+  injectScript
+  onLoading={() => {}}
+  onSuccess={() => {}}
+  onAfterRender={() => {}}
+  onFailure={() => {}}
+/>
+        
      </div>
+     
   );
 }
 
