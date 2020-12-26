fc827ca2cccb1ab8fd5710ff0c74ce0e9354f613
lubayb1 <bryanlubay1@gmail.com>
2020-12-25 22:24:20 -0800

    25 Dec House

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
         <div className="background">
         <h1>Welcome to Cindylus Creations!</h1>
-        <p>Custom    Disney    Las Vegas Golden Knights    Las Vegas Raiders    Los Angeles Dodgers    Los Angeles Lakers</p>
+        <p>Custom    Disney    Las Vegas Golden Knights    Las Vegas Raiders    Los Angeles Dodgers    Los Angeles Lakers    New Years 2021</p>
 
 
         </div>
