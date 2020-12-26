59ba15335b6da323c3454974bf463b61255022df
lubayb1 <bryanlubay1@gmail.com>
2020-12-25 22:22:46 -0800

    25 Dec House

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './images/logo.JPG'
 import './App.css';
 
 function App() {
   return (
         <div className="background">
-        <h1>Welcome to Cindylu's Creations!</h1>
-        <p>Disney   Las Vegas Raiders   Las Vegas Golden Knights  Los Angeles Dodgers   Los Angeles Lakers    Custom</p>
+        <h1>Welcome to Cindylus Creations!</h1>
+        <p>Custom    Disney    Las Vegas Golden Knights    Las Vegas Raiders    Los Angeles Dodgers    Los Angeles Lakers</p>
 
 
         </div>
