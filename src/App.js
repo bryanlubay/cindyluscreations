97868f03440ba2ed2308fbc994576f8635eb86cc
lubayb1 <bryanlubay1@gmail.com>
2020-12-25 21:41:49 -0800

    25 Dec House

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+        <div>
+        <h1>Welcome to Cindylu's Creations!</h1>
+        <a> <h2>About me</h2 ></a>
+        <a> <h2>Gallery</h2> </a>
+        <a> <h2>Contact</h2> </a>
+        </div>
+  );
+}
+
+export default App;
---
+++ src/components/about_me/about_me.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect, Component} from 'react';
+import Login from '../Login/Login';
+import { Redirect } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+import './BB_Product.css'
+
+class App extends Component { 
+   
+  render() { 
+
+
+
+    return (
+      <div className = 'about_me_center'>
+        <h1>About me</h1><br></br>
+    </div>
+
+
+    )
+  }
+  } 
+
+export default App; 
