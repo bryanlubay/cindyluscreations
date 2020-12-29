665df1025a984443c5ba47bc4ea19416cff28fc7
Bryan Lubay <BLubay@ArsenalInc.local>
2020-12-29 15:00:19 -0800

    29 Dec Ars

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import InstagramEmbed from 'react-instagram-embed';
+import Navbar from 'react-bootstrap/Navbar'
 import React from 'react';
 import './App.css';
 
@@ -7,6 +8,26 @@ function App() {
         <div className="background">
         <h1>Welcome to Cindylus Creations!</h1>
         <p className="menu"><b className="menu-item">Custom</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Disney</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Movies</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Music</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">New Years 2021</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Sports</b></p>
+
+        <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="https://cindylus-creations.herokuapp.com/">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="https://cindylus-creations.herokuapp.com/">Home</Nav.Link>
+      <Nav.Link href="#link">About Me</Nav.Link>
+      <NavDropdown title="Sports" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Las Vegas Golden Knights</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Las Vegas Raiders</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Los Angeles Dodgers</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Los Angeles Lakers</NavDropdown.Item>
+        {/* <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
+      </NavDropdown>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+
 
         <InstagramEmbed
   url='https://www.instagr.am/p/CA3aDJ7HlTE/'
