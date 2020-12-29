55ea91bc5591ce2c8f08bf6e9187061dbe22f05f
Bryan Lubay <BLubay@ArsenalInc.local>
2020-12-29 15:08:47 -0800

    29 Dec Ars

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import InstagramEmbed from 'react-instagram-embed';
 import Navbar from 'react-bootstrap/Navbar'
 import React from 'react';
 import './App.css';
+import Nav  from 'react-bootstrap/Nav';
 
 function App() {
   return (
