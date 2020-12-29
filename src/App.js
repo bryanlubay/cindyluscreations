cf77edf7561b63e961dc01b344075a8dabc18a93
Bryan Lubay <BLubay@ArsenalInc.local>
2020-12-29 15:19:42 -0800

    29 Dec Ars

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
         <div className="background">
         <h1>Welcome to Cindylus Creations!</h1>
-        <p className="menu"><b className="menu-item">Custom</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Disney</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Movies</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Music</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">New Years 2021</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Sports</b></p>
+        {/* <p className="menu"><b className="menu-item">Custom</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Disney</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Movies</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Music</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">New Years 2021</b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<b className="menu-item">Sports</b></p> */}
 
         <Navbar bg="light" expand="lg">
   <Navbar.Brand href="https://cindylus-creations.herokuapp.com/">React-Bootstrap</Navbar.Brand>
