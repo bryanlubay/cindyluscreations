9dc588e809a306e2fbf662508118027a8aea8015
Bryan Lubay <BLubay@ArsenalInc.local>
2020-12-30 10:35:24 -0800

    30 Dec Ars

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import Button from 'react-bootstrap/Button'
+import Carousel from 'react-bootstrap/Carousel'
 
 function App() {
   document.title = "Cindylus Creations"
@@ -46,7 +47,45 @@ function App() {
     </Navbar.Collapse>
   </Navbar>
   </Container>
-    <Button>WTF2</Button>
+
+
+  <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="'../src/images/logo.JPG"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="'../src/images/logo.JPG"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="'../src/images/logo.JPG"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
 
 
 
