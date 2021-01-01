7e57d5e344f351320134b5958bf843694daed488
lubayb1 <bryanlubay1@gmail.com>
2020-12-31 21:17:38 -0800

    31 Dec House

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
 
 
 
-              <OverlayTrigger trigger='hover' placement='right'
+              <OverlayTrigger trigger='focus' placement='right'
               overlay={
               <Popover>
                 <Popover.Content>
