c797655172456585d5cd7ef9a4474bb97f7c850e
Bryan Lubay <BLubay@ArsenalInc.local>
2021-01-25 11:25:15 -0800

    25 Jan Ars

--- src/components/about_me/about_me.js
+++ src/components/about_me/about_me.js
@@ -1,13 +1,13 @@
 import React, {useState, useEffect, Component} from 'react';
 
-const About_me = () => {
-    return (
-      <div className='container'>
-        {
-          <h1>About me</h1>
-        }
-      </div>
-    )
-  }
+function About_me() {
+  return (
+    <div>
+      {
+        <h1>About me</h1>
+      }
+    </div>
+  )
+}
 
-export default About_me; 
+export default About_me
