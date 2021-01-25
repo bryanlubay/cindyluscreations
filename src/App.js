10ee9858bdd5fc212bbe5ec03ff9264d372b17ba
Bryan Lubay <BLubay@ArsenalInc.local>
2021-01-25 13:33:54 -0800

    25 Jan Ars

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="https://cindylus-creations.herokuapp.com/">Home</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/about_me">About Me</Nav.Link>
 
               <Nav.Link href="/contact">Contact</Nav.Link>
