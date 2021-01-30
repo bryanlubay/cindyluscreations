2c757e3edaeb60864319bfdb805edf77d7d4e315
lubayb1 <bryanlubay1@gmail.com>
2021-01-29 20:08:37 -0800

    29 Jan House

--- src/components/about_me/about_me.js
+++ src/components/about_me/about_me.js
@@ -5,6 +5,7 @@ function about_me() {
     <div>
         <h1>About me</h1>
         <p>Hello</p>
+        <img src="https://cindyluscreations.s3-us-west-1.amazonaws.com/images/logo.jpg"></img>
     </div>
   )
 }
