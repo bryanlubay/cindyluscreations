d8dcfa5d2adbf6f7ef3d8e2f795f82125343b022
Bryan Lubay <BLubay@ArsenalInc.local>
2021-02-05 14:14:19 -0800

    5 Feb Ars

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, Component} from 'react';
 import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function Contact() {
   return (
