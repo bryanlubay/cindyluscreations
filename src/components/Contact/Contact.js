8e727a303d05688fb39d515ef75cca9324c73725
Bryan Lubay <BLubay@ArsenalInc.local>
2021-02-05 14:14:03 -0800

    5 Feb Ars

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect, Component} from 'react';
+import Form from 'react-bootstrap/Form'
 
 function Contact() {
   return (
@@ -6,6 +7,28 @@ function Contact() {
       
         <h1>Contact</h1>
         <p>Instagram: <a href="https://www.instagram.com/cindylus_creations/" target="_blank">instagram.com/cindylus_creations</a></p>
+
+        <Form>
+        <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="textarea"/>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email"/>
+          </Form.Group>
+
+
+          <Form.Group>
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+
+          
+          <Button type="submit">Submit</Button>
+    
+        </Form>
     </div>
   )
 }
