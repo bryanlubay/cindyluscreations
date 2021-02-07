64e9f911091d4c6b34632dbca8f1765c59452e23
lubayb1 <bryanlubay1@gmail.com>
2021-02-07 11:42:35 -0800

    7 Feb House

--- src/components/about_me/about_me.js
+++ src/components/about_me/about_me.js
@@ -4,8 +4,10 @@ function about_me() {
   return (
     <div>
         <h1>About me</h1>
-        <p></p>
+        <p>I like to make cups, backpacks and a lot of other things!</p>
         <img src="https://cindyluscreations.s3-us-west-1.amazonaws.com/images/logo.jpg"></img>
+        <br></br>
+        <p></p>
     </div>
   )
 }
